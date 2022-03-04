e7d2317906689c6944ce26e758a5a256478951a5
Michael <m261m563@ku.edu>
2022-03-04 15:10:33 -0600

    moved make random guess function to opponent.h

--- EasyAI.cpp
+++ EasyAI.cpp
@@ -15,27 +15,5 @@ void EasyAI::makeMove(Board* enemyBoard, Ship** enemyShips, int numShips, Board*
 }
 
 void EasyAI::makeRandomGuess(Board* enemyBoard, Ship** enemyShips, int numShips, Board* guessBoard) {
-    srand(time(NULL));
-    int col = 0;
-    int row = 0;
-    do {
-        col = rand() % 10;
-        row = rand() % 10;
-    }
-    while(!guessBoard->isValidSpace(col, row));
     
-    col = col + 97;
-    char col_char = col;
-    col = col - 97;
-
-    for(int i=0; i<numShips; i++) {
-        enemyShips[i]->hit(col_char, row);
-    }
-
-    if(!enemyBoard->isValidSpace(col, row)) {
-        guessBoard->updateBoard(col_char, row, '*');
-    }
-    else {
-        guessBoard->updateBoard(col_char, row, 'M');
-    }
 }
--- Opponent.h
+++ Opponent.h
@@ -2,6 +2,8 @@
 #define OPPONENT_H
 
 #include <stdlib.h> //srand, rand
+#include <cstdlib>
+#include <time.h>
 #include "board.h"
 
 class Opponent {
@@ -40,6 +42,32 @@ class Opponent {
             homeBoard->placeShip(nextShip);
         }
     };
+
+    virtual void makeRandomGuess(Board* enemyBoard, Ship** enemyShips, int numShips, Board* guessBoard) {
+        srand(time(NULL));
+        int col = 0;
+        int row = 0;
+        do {
+            col = rand() % 10;
+            row = rand() % 10;
+        }
+        while(!guessBoard->isValidSpace(col, row));
+    
+        col = col + 97;
+        char col_char = col;
+        col = col - 97;
+
+        for(int i=0; i<numShips; i++) {
+            enemyShips[i]->hit(col_char, row);
+        }
+
+        if(!enemyBoard->isValidSpace(col, row)) {
+            guessBoard->updateBoard(col_char, row, '*');
+        }
+        else {
+            guessBoard->updateBoard(col_char, row, 'M');
+        }
+    }
 
     protected:
 
