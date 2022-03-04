547b10682dff853183879858adb9e935c7437cd7
mitchellseifert <mitchellseifert@ku.edu>
2022-03-04 14:53:47 -0600

    Added shot framework for a player.h and player.cpp

--- Player.h
+++ Player.h
@@ -11,6 +11,11 @@ class Player : public Opponent {
         void makeMove(Board* enemyBoard, Ship** enemyShips, int numShips, Board* guessBoard);
         void placeShips(int numShips, Ship** shipList, Board* homeBoard);
 
+        void normalShot(Board* enemyBoard, Ship** enemyShips, int numShips, Board* guessBoard);
+        void bombShot(Board* enemyBoard, Ship** enemyShips, int numShips, Board* guessBoard);
+        void consecutiveShot(Board* enemyBoard, Ship** enemyShips, int numShips, Board* guessBoard, int row, char col);
+        void randomShot(Board* enemyBoard, Ship** enemyShips, int numShips, Board* guessBoard, int row, char col);
+
     private:
         //bool makeGuess(char col, int row, Board* enemyBoard, Ship** enemyShips, int numShips, Board* guessBoard);
 
